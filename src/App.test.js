3e98fd76fb2a3a58202c9780e45b2578a15ba6de
Laura Castillo <lauracasdu@gmail.com>
2022-07-19 16:07:09 +0200

    test home

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Router from '../src/application/Router';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
+test('render home title from navbar', () => {
+  render(<Router />);
+  const tab = screen.getByText(/home/i);
+  expect(tab).toBeInTheDocument();
 });
+
+
+// screen.find -> to find asyncronate elements
+// screen.get -> to find syncronate elements (they should exist in the app)
+// screen.query -> to consult elements that can or can't be into the DOM
+
+// All -> returns all the elements (screen.findAll)
+// By -> returns a single element (screen.findBy)
+
+// eslint-disable-next-line testing-library/no-debuggsing-utils
+// screen.debug(); -> to make a console.log of the things it founds
+
+//react library test
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -8,38 +8,59 @@ import {
 
 function Search({searchMoment, search, setSearch}) {
 
+//   const onInputChange = (e) => {
+//     setSearch(e.target.value);
+//     // console.log(e)
+//   };
 
 
+// const handleSubmitSearch = (e) => {
+//     e.preventDefault();
+//     searchMoment(search);
+//     //console.log(search)
 
-    const onInputChange = (e) => {
-        setSearch(e.target.value);
-        // console.log(e)
-      };
+// };
+
+
+  const onInputChange = (e) => {
+    setSearch(e.target.value);
+    
+      if(setSearch.length > 3){
+        searchMoment(search.toLowerCase().trim())
+      }
+      console.log(search)
+
+
+  };
+
+
+const handleSubmitSearch = (e) => {
+    e.preventDefault();
+    // searchMoment(search);
+
+    searchMoment(search.toLowerCase().trim());
+
+    // console.log(search)
+
+};
+
+
+return (
+
+<CtSearch onSubmit={handleSubmitSearch}>
   
-  
-    const handleSubmitSearch = (e) => {
-        e.preventDefault();
-        searchMoment(search);
-        //console.log(search)
-  
-    };
+         <Input  
+        onChange={onInputChange}
+        type="type"
+        value={search}
+        name="searchMoment"
+        placeholder="Title moment..."
+        />
+        <BtSearch type="submit"><i className="fa-solid fa-magnifying-glass"></i></BtSearch>          
+      </CtSearch>
 
-
-  return (
-
-    <CtSearch onSubmit={handleSubmitSearch}>
-             <Input 
-            onChange={onInputChange}
-            type="text"
-            value={search}
-            name="searchMoment"
-            placeholder="Title moment..."
-            />
-            
-            <BtSearch type="submit"><i className="fa-solid fa-magnifying-glass"></i></BtSearch>          
-          </CtSearch>
-
-  )
+)
 }
+
 
 export default Search
